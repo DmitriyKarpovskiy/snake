894585a90b23524433860236f6c3a63c057da1e4
Dmitriy Karpovskiy <dmkarpovskiy@gmail.com>
2018-12-02 18:27:06 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Point.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+        struct Point
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+            public char ch { get; set; }
+
+            public static bool operator ==(Point a, Point b) =>
+                 (a.x == b.x && a.y == b.y) ? true : false;
+            public static bool operator !=(Point a, Point b) =>
+                (a.x != b.x || a.y != b.y) ? true : false;
+
+             public static implicit operator Point((int, int, char) value) =>
+                new Point { x = value.Item1, y = value.Item2, ch = value.Item3 };
+
+             private void DrawPoint(char ch)
+             {
+                Console.SetCursorPosition(x, y);
+                Console.Write(ch);
+             }
+
+             public void Draw()
+             {
+                DrawPoint(ch);
+             }
+
+             public void Clear()
+             {
+                DrawPoint(' ');
+             }
+        }
+    }
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static readonly int x = 76;
+        static readonly int y = 26;
+        static Food food;
+        static Snake snake;
+        static Timer timer;
+        static Walls walls;
+
+
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(x + 1, y + 1);        //Console set up
+            Console.SetBufferSize(x + 1, y + 1);
+            Console.CursorVisible = false;
+
+            walls = new Walls(x, y, '#');
+
+            snake = new Snake(x / 2, y / 2, 3);
+
+            food = new Food(x, y, '@');
+            food.CreateFood();
+
+            timer = new Timer(Loop, null, 0, 200);
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.Rotation(key.Key);
+                }
+            }
+        }
+
+        static void Loop(object obj)
+        {
+            if (walls.IsHit(snake.GetHead()) || snake.IsHit(snake.GetHead()))
+            {
+                timer.Change(0, Timeout.Infinite);
+            }
+            else if (snake.Eat(food.food))
+            {
+                food.CreateFood();
+            }
+            else
+            {
+                snake.Move();
+            }
+
+        }
+
+
+    }
+}
